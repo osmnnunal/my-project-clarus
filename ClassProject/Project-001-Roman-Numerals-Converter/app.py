7d0b0e89548555cf4cee5272776d2cdbf8bf78b9
osmnnunal <osmnunnal@gmail.com>
2022-09-26 11:14:44 +0300

    update-2

--- ClassProject/Project-001-Roman-Numerals-Converter/app.py
+++ ClassProject/Project-001-Roman-Numerals-Converter/app.py
@@ -14,17 +14,20 @@ def convert(decimal_num):
 
 @app.route('/', methods=['POST', 'GET'])
 def main_post():
-    if request.method == 'POST':
+    if request.method == 'POST':  #bu number sayısını çek 
         alpha = request.form['number']
-        if not alpha.isdecimal():
+        if not alpha.isdecimal(): #bu sayı desimal değilse (negatif, float) index html kirala ve hata verdir(not valid=True) 
             return render_template('index.html', developer_name='Osman', not_valid=True)
         number = int(alpha)
-        if not 0 < number < 4000:
+        if not 0 < number < 4000: #sayı bu aralıkta değilse index.html kirala yine hata mesajı verdir.
             return render_template('index.html', developer_name='Osman', not_valid=True)
         return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Osman')
-    else:
+    #eğer üstteki iki koşulda geçerli değilse demekki sonuç doğru result.html kirala
+    #number decimal = kutucuktan çekilen sayı sonra alpha ya atanan sayı (18.satır)
+    # numberroman = convert fonk.girecek olan sayı(5.satır)number_roman = convert(number)
+    else:  # post değilse yani get ise ana sayfayı karşımıza çıkaracak index.html den. notvalid false olduğu için alttaki hata çıkmıyor.
         return render_template('index.html', developer_name='Osman', not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
